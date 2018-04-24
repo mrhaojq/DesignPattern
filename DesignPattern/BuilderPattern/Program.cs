69d7dd09ed2d958816324ed978a6b58943022a13
mrhaojq <haojiaqi1119@126.com>
2018-04-24 22:38:37 +0800

    Add Builder Pattern

---
+++ DesignPattern/BuilderPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TextBuilder textBuilder = new TextBuilder();
+                Director director = new Director(textBuilder);
+                director.Construct();
+                string result = textBuilder.GetResult();
+                WriteLine(result);
+
+                HTMLBuilder htmlBuilder = new HTMLBuilder();
+                Director hDirector = new Director(htmlBuilder);
+                hDirector.Construct();
+                string hResult = htmlBuilder.GetResult();
+                WriteLine(hResult);
+
+                ReadKey();
+            }
+            catch (Exception ex)
+            {
+
+                //throw;
+            }
+            
+        }
+    }
+}
---
+++ DesignPattern/BuilderPattern/TextBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    public class TextBuilder : Builder
+    {
+        private StringBuilder _buffer = new StringBuilder();
+
+        public string GetResult()
+        {
+            return _buffer.ToString();
+        }
+
+        public override void Close()
+        {
+            _buffer.Append("==================================================\n");
+        }
+
+        public override void MakeItems(string[] items)
+        {
+            foreach (var item in items)
+            {
+                _buffer.Append($"   {item}\n");
+            }
+            _buffer.Append("\n");
+        }
+
+        public override void MakeString(string str)
+        {
+            _buffer.Append($"● {str}\n");
+            _buffer.Append("\n");
+        }
+
+        public override void MakeTitle(string title)
+        {
+            _buffer.Append("==================================================\n");
+            _buffer.Append($"『{title}』");
+            _buffer.Append("\n");
+        }
+    }
+}
