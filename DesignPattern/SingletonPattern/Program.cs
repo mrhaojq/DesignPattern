87318368664ab134ff165eaa28a281940ded8d31
mrhaojq <haojiaqi1119@126.com>
2018-04-24 20:24:28 +0800

    Add Singleton Pattern

---
+++ DesignPattern/SingletonPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("start");
+            Singleton obj1 = Singleton.getInstance();
+            //Singleton obj2 = new Singleton(); 构造函数设置成私有的，不能通过new 创建该类的实例
+            Singleton obj2 = Singleton.getInstance();
+
+            if (obj1.Equals(obj2))
+            {
+                WriteLine("obj1与obj2是相同的实例。");
+            }
+            else
+            {
+                WriteLine("obj1与obj2不是相同的实例。");
+            }
+            WriteLine("end");
+            ReadKey();
+        }
+    }
+}
---
+++ DesignPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SingletonPattern
+{
+   public class Singleton
+    {
+        //私有静态
+        private static Singleton _singleton = new Singleton();
+
+        //将构造函数设置成私有的，阻止通过外部创建该类的实例
+        private Singleton()
+        {
+            WriteLine("生成了一个实例。");
+        }
+
+        /// <summary>
+        ///提供静态的获取该私有实例的方法，只能通过这一种方式获取创建的私有实例
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton getInstance()
+        {
+            return _singleton;
+        }
+    }
+}
