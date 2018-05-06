fd1620f6cc9694fdec7d5f4d4a0211584f3bff28
mrhaojq <haojiaqi1119@126.com>
2018-05-06 20:43:14 +0800

    StrategyPattern

---
+++ DesignPattern/StrategyPattern/RandomNumberHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace StrategyPattern
+{
+     public  class RandomNumberHelper
+    {
+        public static int GetRandomInt()
+        {
+            //RNGCryptoServiceProvider 和 RandomNumberGenerator 都是OK的
+            byte[] byteCsp = new byte[4];
+
+            //RNGCryptoServiceProvider csp = new RNGCryptoServiceProvider();
+            //csp.GetBytes(byteCsp);
+
+            RandomNumberGenerator generator = RandomNumberGenerator.Create();
+            generator.GetBytes(byteCsp);
+
+            int number = BitConverter.ToInt32(byteCsp,0);
+            return number;
+            //Console.WriteLine(BitConverter.ToString(byteCsp));
+        }
+    }
+}
---
+++ DesignPattern/StrategyPattern/WinningStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class WinningStrategy : Strategy
+    {
+        Random random;
+        bool won = false;
+        Hand prevHand;
+
+        public WinningStrategy(int seed)
+        {
+            random = new Random(seed);
+        }
+        public Hand NextHand()
+        {
+            if (!won){
+                int r = random.Next
+            (3);
+                prevHand = Hand.GetHand(r);
+            }
+            return prevHand;
+        }
+
+        public void Study(bool win)
+        {
+            won = win;
+        }
+    }
+}
